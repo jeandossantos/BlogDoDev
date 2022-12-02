2f9a62d84f3783f2579c632171fe1623f31081b5
Jean F. dos Santos <jeanddg@hotmail.com>
2022-12-02 15:30:04 -0300

    🎉 initial commit

---
+++ backend/src/index.ts
@@ -0,0 +1,35 @@
+import express, { NextFunction, Request, Response } from 'express';
+import { CustomException } from './app/exceptions/CustomException';
+import { ZodError } from 'zod';
+
+const app = express();
+app.use(express.json());
+
+app.use(
+  (
+    error: CustomException | ZodError | Error,
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ) => {
+    if (error instanceof CustomException) {
+      return res.status(error.code).json({
+        message: error.message,
+        code: error.code,
+        name: error.name,
+      });
+    }
+
+    if (error instanceof ZodError) {
+      return res.status(400).json(error);
+    }
+
+    console.error(error.message || error);
+
+    return res.status(500).json({
+      code: 500,
+      message: 'Unexpected error',
+    });
+  }
+);
+export { app };
