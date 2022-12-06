e45567569aae2441ef93d0443aa1e2fa8a743e6c
Jean F. dos Santos <jeanddg@hotmail.com>
2022-12-06 18:34:03 -0300

    ✅ should create a article test

---
+++ backend/__tests__/unit/article/createArticle.test.ts
@@ -0,0 +1,37 @@
+import { ArticleService } from '../../../src/app/article/ArticleService';
+import { IUser } from '../../../src/app/user/IUserRepository';
+import { ArticleRepoInMemory } from '../../../src/inMemory/ArticleRepoInMemory';
+import { UserRepoInMemory } from '../../../src/inMemory/UserRepoInMemory';
+import { TagRepoInMemory } from '../../../src/inMemory/TagRepoInMemory';
+import { ITag } from '../../../src/app/tag/ITagRepository';
+
+const articleService = new ArticleService(new ArticleRepoInMemory());
+
+let user: IUser;
+let tag: ITag;
+
+beforeAll(async () => {
+  user = await new UserRepoInMemory().create({
+    username: 'bob',
+    email: 'bob@example.com',
+    password: 'password',
+    createdAt: new Date(),
+  });
+
+  tag = await new TagRepoInMemory().create('TagForArticle');
+});
+
+describe('Create a article', () => {
+  it('should create an article', async () => {
+    const article = await articleService.create({
+      title: 'My Article',
+      content:
+        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries.",
+      imageUrl: 'imageUrl.png',
+      tags: [tag.id!],
+      authorId: user.id!,
+    });
+
+    expect(article.id).toBeDefined();
+  });
+});
