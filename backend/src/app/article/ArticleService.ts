815124dffcc181e18b5e27a1d322e5929c4f565c
Jean F. dos Santos <jeanddg@hotmail.com>
2022-12-06 18:33:36 -0300

    ✨ (ArticleService): add create article method

---
+++ backend/src/app/article/ArticleService.ts
@@ -0,0 +1,30 @@
+import { IArticle, IArticleRepository } from './IArticle';
+import { z } from 'zod';
+
+type CreateArticleProps = Omit<IArticle, 'id'>;
+
+export class ArticleService {
+  constructor(private articleRepository: IArticleRepository) {}
+
+  async create(props: CreateArticleProps) {
+    const { title, imageUrl, content, authorId, tags } = z
+      .object({
+        title: z.string().min(2),
+        imageUrl: z.string().optional(),
+        content: z.string(),
+        authorId: z.string(),
+        tags: z.array(z.string()),
+      })
+      .parse(props);
+
+    const article = await this.articleRepository.create({
+      title,
+      imageUrl,
+      content,
+      authorId,
+      tags,
+    });
+
+    return article;
+  }
+}
