0dbe853ac4ff35f7ba4ded4af0d150bfb0574a51
Jean F. dos Santos <jeanddg@hotmail.com>
2022-12-06 19:11:55 -0300

    should not create an article without an owner

--- backend/__tests__/unit/article/createArticle.test.ts
+++ backend/__tests__/unit/article/createArticle.test.ts
@@ -82,4 +82,16 @@ describe('Create a article', () => {
 
     await expect(article).rejects.toThrowError(ZodError);
   });
+
+  it('should not create an article without an owner', async () => {
+    const article = articleService.create({
+      title: 'My Article',
+      content:
+        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries.",
+      tags: [],
+      authorId: '',
+    });
+
+    await expect(article).rejects.toThrowError(ZodError);
+  });
 });
