a60e31739e236348a1341d1e3ac9ce6d4abf98e5
Jean F. dos Santos <jeanddg@hotmail.com>
2022-12-06 18:54:18 -0300

    ✅ should not create an article without title

--- backend/__tests__/unit/article/createArticle.test.ts
+++ backend/__tests__/unit/article/createArticle.test.ts
@@ -58,4 +58,16 @@ describe('Create a article', () => {
 
     await expect(article).rejects.toThrowError(ZodError);
   });
+
+  it('should not create an article without title', async () => {
+    const article = articleService.create({
+      title: '',
+      content:
+        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries.",
+      tags: [tag.id!],
+      authorId: user.id!,
+    });
+
+    await expect(article).rejects.toThrowError(ZodError);
+  });
 });
