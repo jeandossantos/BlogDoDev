8d6d81494cffb54b8f1c4489d21251b5d7f2f184
Jean F. dos Santos <jeanddg@hotmail.com>
2022-12-09 18:04:20 -0300

    ✅ should remove a user

---
+++ backend/__tests__/integration/user/removeUser.spec.ts
@@ -0,0 +1,32 @@
+import { Client } from 'pg';
+import request from 'supertest';
+import { app } from '../../../src';
+import { prisma } from '../../../src/connection/prisma';
+
+const client = new Client({
+  connectionString: process.env.DATABASE_URL,
+});
+
+let user;
+
+beforeAll(async () => {
+  await client.connect();
+  await client.query('DELETE FROM users');
+  await client.end();
+
+  user = await prisma.user.create({
+    data: {
+      username: 'user to be updated',
+      email: 'user@example.com',
+      password: 'password',
+    },
+  });
+});
+
+describe('should remove a user', () => {
+  it('should remove the user', async () => {
+    const response = await request(app).delete(`/users/${user.id}`);
+
+    expect(response.status).toBe(200);
+  });
+});
