533c2efb2de5cb3e664bdc7d11ae44c9f3ae7778
Jean F. dos Santos <jeanddg@hotmail.com>
2022-12-11 11:42:13 -0300

    ✅ should create a tag

---
+++ backend/__tests__/integration/tag/createTag.spec.ts
@@ -0,0 +1,60 @@
+import jwt from 'jsonwebtoken';
+import { describe, expect, test } from '@jest/globals';
+import request from 'supertest';
+import { Client } from 'pg';
+
+import { UserRepository } from './../../../src/app/user/UserRepository';
+import { UserService } from './../../../src/app/user/UserService';
+import { app } from '../../../src';
+import { prisma } from '../../../src/connection/prisma';
+
+let userId: string;
+
+beforeAll(async () => {
+  const client = new Client({
+    connectionString: process.env.DATABASE_URL,
+  });
+
+  await client.connect();
+  await client.query('delete from tags');
+  await client.end();
+
+  userId = (await createUser('user-test-tag@test.com')).id!;
+});
+
+async function createUser(email: string) {
+  const userRepository = new UserRepository();
+  const userService = new UserService(userRepository);
+
+  return userService.create({
+    username: 'my user test',
+    email,
+    password: 'password',
+    confirmPassword: 'password',
+  });
+}
+
+async function createTag(tagName: string) {
+  return await prisma.tag.create({
+    data: { name: tagName },
+  });
+}
+
+const generateToken = function (id: string) {
+  return jwt.sign({ id: id }, process.env.SECRET_KEY!);
+};
+
+describe('create tag', () => {
+  it('should create a tag', async () => {
+    const token = generateToken(userId);
+
+    const response = await request(app)
+      .post('/tags')
+      .set('Authorization', `Bearer ${token}`)
+      .send({ name: 'some tag' });
+
+    expect(response.status).toBe(201);
+    expect(response.body).toHaveProperty('id');
+    expect(response.body.name).toStrictEqual('some tag');
+  });
+});
