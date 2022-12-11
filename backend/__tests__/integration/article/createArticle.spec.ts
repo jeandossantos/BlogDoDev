fcf12b2f6b86f3fab14d2ec0e5898aadf307ddbe
Jean F. dos Santos <jeanddg@hotmail.com>
2022-12-11 19:32:04 -0300

    ✅ should not create article with invalid title

--- backend/__tests__/integration/article/createArticle.spec.ts
+++ backend/__tests__/integration/article/createArticle.spec.ts
@@ -62,4 +62,26 @@ describe('Create article', () => {
 
     expect(response.body).toHaveProperty('id');
   });
+
+  it('should not create article with invalid title', async () => {
+    const tag = await prisma.tag.create({
+      data: { name: 'someTag-test' },
+    });
+
+    const token = generateToken(userId);
+
+    const response = await request(app)
+      .post('/articles')
+      .set('Authorization', `Bearer ${token}`)
+      .send({
+        authorId: userId,
+        title: '',
+        description: 'some description',
+        content:
+          "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries",
+        tags: [tag.id],
+      });
+
+    expect(response.body.issues[0].code).toBe('too_small');
+  });
 });
