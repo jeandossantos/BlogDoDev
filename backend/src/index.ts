13540d51f6d57454ff5864a3d2da684ac8224ace
Jean F. dos Santos <jeanddg@hotmail.com>
2022-12-16 12:30:29 -0300

    ➕ add swagger to application

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,6 +1,9 @@
 import 'express-async-errors';
 import 'dotenv/config';
-import path from 'path';
+
+import swaggerUi from 'swagger-ui-express';
+import path from 'node:path';
+
 import express, { NextFunction, Request, Response } from 'express';
 import { CustomException } from './exceptions/CustomException';
 import { ZodError } from 'zod';
@@ -8,13 +11,21 @@ import { ZodError } from 'zod';
 import { routes as userRoutes } from './routes/user.routes';
 import { routes as tagRoutes } from './routes/tag.routes';
 import { routes as articleRoutes } from './routes/article.routes';
+import swaggerDocument from './swagger.json';
 
 const app = express();
+
 app.use(express.json());
 app.use('/static', express.static(path.resolve(__dirname, '..', 'public')));
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use(userRoutes);
 app.use(tagRoutes);
 app.use(articleRoutes);
+app.use('/terms', (req, res) =>
+  res.json({
+    terms: 'This are my terms!',
+  })
+);
 app.use(
   (
     error: CustomException | ZodError | Error,
