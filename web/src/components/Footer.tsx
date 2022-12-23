fa02ad2ae69ed43687aa4d0f75f1e953ddf4dc6e
Jean F. dos Santos <jeanddg@hotmail.com>
2022-12-23 09:40:10 -0300

    create Footer component

---
+++ web/src/components/Footer.tsx
@@ -0,0 +1,9 @@
+export function Footer() {
+  return (
+    <footer className="row-span-1 col-span-2 flex justify-center items-center bg-zinc-800 text-zinc-100 font-medium mt-6">
+      <span className="font-[Lalezar] text-sm">
+        Copyright &copy; 2022 BlogDoDev Inc.
+      </span>
+    </footer>
+  );
+}
