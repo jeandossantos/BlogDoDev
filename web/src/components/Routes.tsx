3b549d017d5e996064043c5ed543b78302046570
Jean F. dos Santos <jeanddg@hotmail.com>
2022-12-26 18:50:26 -0300

    add new tags

--- web/src/components/Routes.tsx
+++ web/src/components/Routes.tsx
@@ -1,16 +1,21 @@
 import { Routes, Route } from 'react-router-dom';
 import { ArticleForm } from '../pages/ArticleForm';
-import { ArticleItem } from '../pages/ArticleItem';
+import { ArticleById } from '../pages/ArticleById';
 
 import { Home } from '../pages/Home';
 import { Profile } from '../pages/Profile';
 import { PrivateRoute } from './PrivateRoute';
+import { ArticlesFound } from '../pages/ArticlesFound';
+import { ArticlesByTag } from '../pages/ArticlesByTag';
 
 export default function () {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
-      <Route path="/articles/:articleId" element={<ArticleItem />} />
+      <Route path="/articles/:articleId" element={<ArticleById />} />
+      <Route path="/articles/result" element={<ArticlesFound />} />
+      <Route path="/articles/tag/:tagId" element={<ArticlesByTag />} />
+
       <Route
         path="/articles"
         element={
