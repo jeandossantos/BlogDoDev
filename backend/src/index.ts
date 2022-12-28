f535d72a01dd69e7f04dd518656bea69d50d5c85
Jean F. dos Santos <jeanddg@hotmail.com>
2022-12-28 10:35:07 -0300

    remove swagger

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,7 +1,6 @@
 import 'express-async-errors';
 import 'dotenv/config';
 
-import swaggerUi from 'swagger-ui-express';
 import path from 'node:path';
 import cors from 'cors';
 import express, { NextFunction, Request, Response } from 'express';
@@ -13,7 +12,6 @@ import helmet from 'helmet';
 import { routes as userRoutes } from './routes/user.routes';
 import { routes as tagRoutes } from './routes/tag.routes';
 import { routes as articleRoutes } from './routes/article.routes';
-import swaggerDocument from './swagger.json';
 
 const app = express();
 
@@ -21,16 +19,11 @@ app.use(express.json());
 app.use(cors());
 app.use(helmet());
 app.use('/static', express.static(path.resolve(__dirname, '..', 'public')));
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use(morgan('dev'));
 app.use(userRoutes);
 app.use(tagRoutes);
 app.use(articleRoutes);
-app.use('/terms', (req, res) =>
-  res.json({
-    terms: 'This are my terms!',
-  })
-);
+
 app.use(
   (
     error: CustomException | ZodError | Error,
